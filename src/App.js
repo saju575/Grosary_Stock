99e90dd0a43d9c8a8a228912b6129a144300169b
saju1121 <sakilahmedsaju1121@gmail.com>
2022-05-06 19:29:40 +0600

    regiter and login page implement

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import Footer from "./Pages/Common/Footer/Footer";
 import Navbar from "./Pages/Common/Navbar/Navbar";
 import Home from "./Pages/Home/Home/Home";
+import Login from "./Pages/Login/Login/Login";
+import Register from "./Pages/Login/Register/Register";
 
 function App() {
 	return (
@@ -10,6 +12,8 @@ function App() {
 			<Navbar />
 			<Routes>
 				<Route path="/" element={<Home />}></Route>
+				<Route path="/login" element={<Login />}></Route>
+				<Route path="/register" element={<Register />}></Route>
 			</Routes>
 			<Footer />
 		</div>
