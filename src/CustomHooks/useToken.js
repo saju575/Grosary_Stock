f6f892b56bd3da4dfb7fc25ff624dc18ebb4f73d
saju575 <sakilahmedsaju575@gmail.com>
2023-07-14 20:33:12 +0600

    login backend link fix

--- src/CustomHooks/useToken.js
+++ src/CustomHooks/useToken.js
@@ -1,30 +1,29 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
+import axios from "../Function/url";
 
 const useToken = (user) => {
-	const [token, setToken] = useState("");
-	// const [load, setLoad] = useState(false);
-	useEffect(() => {
-		const getToken = async () => {
-			//setLoad(true);
-			const email = user?.user?.email;
-			if (email) {
-				const { data } = await axios.post(
-					"https://intense-wave-00513.herokuapp.com/login",
-					{
-						email: email,
-					}
-				);
-				setToken(data.accessToken);
-				localStorage.setItem("accessToken", data.accessToken);
-				// if (data?.accessToken) {
-				// 	setLoad(false);
-				// }
-			}
-		};
-		getToken();
-	}, [user]);
-	return [token];
+  const [token, setToken] = useState("");
+  // const [load, setLoad] = useState(false);
+  useEffect(() => {
+    const getToken = async () => {
+      //setLoad(true);
+      const email = user?.user?.email;
+      try {
+        if (email) {
+          const { data } = await axios.post("/login", {
+            email: email,
+          });
+          setToken(data.accessToken);
+          localStorage.setItem("accessToken", data.accessToken);
+          // if (data?.accessToken) {
+          // 	setLoad(false);
+          // }
+        }
+      } catch (error) {}
+    };
+    getToken();
+  }, [user]);
+  return [token];
 };
 
 export default useToken;
